cbdfca648e9196c7299b4980a3c45097a9edc7dc
Temi <kanyinsolao@nurtur.tech>
2024-06-23 22:44:18 +0100

    use intersections

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const observer = new IntersectionObserver((entries) => {    
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            // observer.unobserve(entry.target);
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+newFunction(true);
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((element) => {
-    observer.observe(element);
-});
+/**
+ * 
+ * @param {boolean} showAgain 
+ */
+function newFunction(showAgain) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                // observer.unobserve(entry.target);
+            } else {
+                if (showAgain)
+                    entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    hiddenElements.forEach((element) => {
+        observer.observe(element);
+    });
+
+    const delayedChildren = document.querySelectorAll('.hidden__transition__delay');
+    delayedChildren.forEach((element, index) => {
+        element.style.transitionDelay = `${index * 200}ms`;
+    });
+}
